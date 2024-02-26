b59b3f36f23efce96e42b0804d95d82192b94903
Zidan227 <muhammadzidhanzidan@gmail.com>
2024-02-26 22:14:51 +0700

    search baru

--- DashboardMember/dashboard.php
+++ DashboardMember/dashboard.php
@@ -22,27 +22,23 @@ require "../config/config.php";
 $buku = queryReadData("SELECT * FROM buku");
 //search buku
 if (isset($_POST["search"])) {
-  $buku = search($_POST["keyword"]);
-}
-//read buku informatika
-if (isset($_POST["informatika"])) {
-  $buku = queryReadData("SELECT * FROM buku WHERE kategori = 'informatika'");
-}
-//read buku bisnis
-if (isset($_POST["bisnis"])) {
-  $buku = queryReadData("SELECT * FROM buku WHERE kategori = 'bisnis'");
-}
-//read buku filsafat
-if (isset($_POST["filsafat"])) {
-  $buku = queryReadData("SELECT * FROM buku WHERE kategori = 'filsafat'");
-}
-//read buku novel
-if (isset($_POST["novel"])) {
-  $buku = queryReadData("SELECT * FROM buku WHERE kategori = 'novel'");
-}
-//read buku sains
-if (isset($_POST["sains"])) {
-  $buku = queryReadData("SELECT * FROM buku WHERE kategori = 'sains'");
+  $buku = queryReadData("SELECT * FROM buku");
+  //search buku
+  if (isset($_POST["search"])) {
+    $buku = search($_POST["keyword"]);
+  }
+  //read buku informatika
+  if (isset($_POST["Dongeng"])) {
+    $buku = queryReadData("SELECT * FROM buku WHERE kategori = 'Dongeng'");
+  }
+  //read buku bisnis
+  if (isset($_POST["Komik"])) {
+    $buku = queryReadData("SELECT * FROM buku WHERE kategori = 'Komik'");
+  }
+  //read buku filsafat
+  if (isset($_POST["Novel"])) {
+    $buku = queryReadData("SELECT * FROM buku WHERE kategori = 'Novel'");
+  }
 }
 ?>
 
